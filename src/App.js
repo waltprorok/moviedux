effe4761dd359bd2030043d0a14cfb4718948d08
Walter Prorok <waltprorok@gmail.com>
2025-02-13 16:51:31 -0500

    Moviedux: created footer component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import './styles.css';
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
     return (
@@ -10,9 +11,7 @@ function App() {
             </div>
 
 
-            <footer className="footer">
-                <p className='footer'>Footer content here.</p>
-            </footer>
+            <Footer></Footer>
         </div>
     );
 }
